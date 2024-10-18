748ba677425bc56b18d74be33c66d11bc9f4b7fd
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2024-10-18 13:00:17 -0700

    building blocks

--- src/services/Catalog/Products/CreateProduct/CreateProductHandle.cs
+++ src/services/Catalog/Products/CreateProduct/CreateProductHandle.cs
@@ -1,3 +1,26 @@
+using MediatR;
+
 namespace Catalog.API.Products.CreateProduct;
 
-public class CreateProductHandle { }
+public record CreateProductCommand(
+    Guid Id,
+    string Name,
+    List<string> Categories,
+    string Description,
+    string ImageFile,
+    decimal Price
+) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandle : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    // Handle business logic for creating a product
+    public Task<CreateProductResult> Handle(
+        CreateProductCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        throw new NotImplementedException();
+    }
+}
