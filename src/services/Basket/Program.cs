fa1c70748a35f774e8c903e6c3a2e765b20b8830
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2024-10-30 09:06:48 -0700

    distributed cache and health checks

--- src/services/Basket/Program.cs
+++ src/services/Basket/Program.cs
@@ -1,4 +1,6 @@
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,18 +25,31 @@ builder
     .UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis")!;
+    options.InstanceName = "Basket";
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder
+    .Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 #endregion
 
 #region Builder
 
 var app = builder.Build();
-
-app.UseExceptionHandler(options => { });
-
 app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks(
+    "/health",
+    new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse }
+);
 
 app.Run();
 #endregion
