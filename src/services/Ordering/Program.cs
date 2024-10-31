732df29d5233c2524f0f7a40809fe27610973af7
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2024-10-31 14:28:52 -0700

    Update Program.cs

--- src/services/Ordering/Program.cs
+++ src/services/Ordering/Program.cs
@@ -1,4 +1,11 @@
+#region Builder Configuration
 var builder = WebApplication.CreateBuilder(args);
+
+#endregion
+
+#region Services Configuration
 var app = builder.Build();
 
 app.Run();
+
+#endregion
