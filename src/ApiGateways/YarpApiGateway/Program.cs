b4739f0f448c021c16a88021abc2303af193a260
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2024-11-12 12:58:30 -0800

    add rate limiter to requests

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,8 +1,25 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter(
+        "fixed",
+        options =>
+        {
+            // Means on the span of 10 seconds, only 5 requests are allowed
+            options.Window = TimeSpan.FromSeconds(10);
+            options.PermitLimit = 5;
+        }
+    );
+});
+
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
